a665c17923026703b92c94d4bbdc0c1a01960a1c
overdrivenpotato <marko.mijalkovic97@gmail.com>
2014-04-28 18:13:56 -0400

    Added function skeletons

--- Hearts.cpp
+++ Hearts.cpp
@@ -12,6 +12,51 @@ int countSuit(int hand[], int cards, int suit);
 string cardName(int card);
 void showHand(int hand[], int size);
 
+int playHand(int scores[])
+{
+
+}
+
+void shuffleAndDeal()
+{
+
+}
+
+void leadToTrick(int hand[], int size, int& card, bool human, bool broken)
+{
+
+}
+
+void chooseCard(int hand[], int size, int suit, int& card, bool human)
+{
+
+}
+
+int trickWinner(int trick[], int leader)
+{
+
+}
+
+int pointCount(int trick[])
+{
+
+}
+
+int countSuit(int hand[], int cards, int suit)
+{
+
+}
+
+string cardName(int card)
+{
+
+}
+
+void showHand(int hand[], int size)
+{
+
+}
+
 int main()
 {
 
